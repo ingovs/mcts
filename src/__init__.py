06710c5eab44328b87a95f26320e7beef01b55fa
ingo <ingoseckelmann@outlook.com.br>
2025-10-19 17:11:12 -0300

    chore: remove unused configuration imports from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,28 +5,10 @@ A configurable implementation of MCTS with adjustable depth and breadth paramete
 """
 
 from .mcts_chess import ChessMCTS, MCTSConfig, MCTSNode
-from .config import (
-    SearchConfig,
-    get_config_by_strength,
-    create_custom_config,
-    FAST_CONFIG,
-    BALANCED_CONFIG,
-    DEEP_CONFIG,
-    TOURNAMENT_CONFIG,
-    ALPHAZERO_CONFIG,
-)
 
 __version__ = "1.0.0"
 __all__ = [
     "ChessMCTS",
     "MCTSConfig",
     "MCTSNode",
-    "SearchConfig",
-    "get_config_by_strength",
-    "create_custom_config",
-    "FAST_CONFIG",
-    "BALANCED_CONFIG",
-    "DEEP_CONFIG",
-    "TOURNAMENT_CONFIG",
-    "ALPHAZERO_CONFIG",
 ]
